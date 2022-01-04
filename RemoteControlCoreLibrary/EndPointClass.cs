103657f65024c3980546c8dd34316507f8778e9c
Andy <musictopia2@hotmail.com>
2022-01-04 09:55:53 -0700

    Created sample implmentation of interface for the url of the remote control hub.  This is intended to be a shared library to be used by both client and host.  Obviously, you can create other classes and register them instead in the dependency injection system.

---
+++ RemoteControlCoreLibrary/EndPointClass.cs
@@ -0,0 +1,10 @@
+﻿using RemoteControl.CoreLibrary.Helpers;
+namespace RemoteControlCoreLibrary;
+public class EndPointClass : IRemoteControlEndPoint
+{
+    string IRemoteControlEndPoint.EndPointAddress => "http://192.168.0.150:5252/remotecontrolhub";
+    //notes:  make sure to change the 192.168.0.150 to the ip address you choose to use that all computers on your network can use.
+    //also, make sure to change 5252 to the random port assigned when you create your server project.
+    //the purpose of this interface is you use the interface and whatever you put here will be the url used.  allows for unlimited possibilities of where the hub is.
+    //this is the server.  both the client and host care about the server, not each other.
+}
